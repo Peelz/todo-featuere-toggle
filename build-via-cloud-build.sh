89a4f1775567c6da3436abc683fb9b7cba0119d7
Anucha M <monopeelz@gmail.com>
2022-06-17 02:15:43 +0700

    update script

--- build-via-cloud-build.sh
+++ build-via-cloud-build.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-echo "building $GO_PIPELINE_LABEL"
-
 artifactRegion="asia-southeast1-docker.pkg.dev"
 gcpProjectName="tdg-dh-truehealth-core-nonprod"
 repository="cossack-docker"
 artifactName="poc-go-todo-service"
+
 tag="latest"
 if [[ ! -z "$GO_PIPELINE_LABEL" ]]; then
     tag="$GO_PIPELINE_LABEL"
 fi
-prefixArtifactName="${artifactRegion}/${gcpProjectName}/${repository}/${artifactName}"
 
-cat deployment/cloudbuild.yaml
+imageName="${artifactRegion}/${gcpProjectName}/${repository}/${artifactName}"
 
-gcloud builds submit \
-    --config=build
-    --suppress-logs 
+cat deployment/cloudbuild.yaml |
+    sed s/{{.tag}}/${tag}/g |
+    sed s/{{.image}}/${imageName}/g >cloudbuild.yaml
+
+gcloud builds submit
