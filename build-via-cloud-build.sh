185e0ced9701eb4c00a36a429203a877922f28ce
Anucha M <monopeelz@gmail.com>
2022-06-16 22:14:23 +0700

    fix artifact region

--- build-via-cloud-build.sh
+++ build-via-cloud-build.sh
@@ -6,8 +6,11 @@ artifactRegion="asia-southeast1-docker.pkg.dev"
 gcpProjectName="tdg-dh-truehealth-core-nonprod"
 repository="cossack-docker"
 artifactName="poc-go-todo-service"
-tag="$1"
-prefixArtifactName="${asia-southeast1-docker.pkg.dev}/${gcpProjectName}/${repository}/${artifactName}"
+tag="latest"
+if [[ ! -z "$GO_PIPELINE_LABEL" ]]; then
+    tag="$GO_PIPELINE_LABEL"
+fi
+prefixArtifactName="${artifactRegion}/${gcpProjectName}/${repository}/${artifactName}"
 
 gcloud builds submit \
     --tag "${prefixArtifactName}:${tag}"
