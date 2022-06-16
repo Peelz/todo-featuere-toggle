214010942150fff6febb33256db0c84021aed662
Anucha M <monopeelz@gmail.com>
2022-06-16 22:06:16 +0700

    fix artifact name

--- build-via-cloud-build.sh
+++ build-via-cloud-build.sh
@@ -2,7 +2,7 @@
 
 artifactRegion="asia-southeast1-docker.pkg.dev"
 gcpProjectName="tdg-dh-truehealth-core-nonprod"
-repository="tdg-dh-truehealth-core-nonprod"
+repository="cossack-docker"
 artifactName="poc-go-todo-service"
 tag="$1"
 prefixArtifactName="${asia-southeast1-docker.pkg.dev}/${gcpProjectName}/${repository}/${artifactName}"
